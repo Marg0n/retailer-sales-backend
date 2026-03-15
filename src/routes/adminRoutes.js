2911a5f3ba178b1257a56a00dad74714aac73f64
Margon  <monondcosta@gmail.com>
2026-03-15 16:56:58 +0600

    Add: Added bulkAssignRetailers to adminRoutes.

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import upload from "../middleware/uploadMiddleware.js";
-import { importRetailers } from "../controllers/adminController.js";
+import { bulkAssignRetailers, importRetailers } from "../controllers/adminController.js";
 
 const router = express.Router();
 
 router.post("/retailers/import", upload.single("file"), importRetailers);
+router.post("/assignments/bulk", bulkAssignRetailers);
 
 export default router;
