ece96fb93564f80e2f93c0e99fb4ab76780ba921
Margon  <monondcosta@gmail.com>
2026-03-15 16:45:56 +0600

    Fix: Fixed adminRoutes wrong import.

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { upload } from "../middleware/uploadMiddleware.js";
+import upload from "../middleware/uploadMiddleware.js";
 import { importRetailers } from "../controllers/adminController.js";
 
 const router = express.Router();
